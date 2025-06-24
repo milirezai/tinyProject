a0b1055028a21d4606ecf4f9f8f871a04e305999
milirezai <miladrezaix@gmail.com>
2025-06-24 18:01:31 +0430

    The model part of the project is finished

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -1,1 +1,27 @@
 <?php
+ namespace Application\Controllers;
+
+ class article extends Controllers
+ {
+    public function index()
+    {
+    }
+    public function create()
+    {
+    }
+    public function store()
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update($id)
+    {
+    }
+    public function destroy($id)
+    {
+    }
+ }
--- application/controllers/category.php
+++ application/controllers/category.php
@@ -1,2 +1,27 @@
 <?php
- 
+ namespace Application\Controllers;
+
+ class category extends Controllers
+ {
+    public function index()
+    {
+    }
+    public function create()
+    {
+    }
+    public function store()
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update($id)
+    {
+    }
+    public function destroy($id)
+    {
+    }
+ }
